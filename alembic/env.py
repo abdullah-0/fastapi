f65549ecbe6339ff22d590207301a2ca399fd5b0
Abdullah <mabdullah.tahirk@gmail.com>
2025-05-06 14:19:20 +0500

    models added to alembic env

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,12 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from core.config import Base, DATABASE_URL
 
-# models are required to make versions
+
+# Imports to support database version control
+from core.config import Base, DATABASE_URL
+import models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
